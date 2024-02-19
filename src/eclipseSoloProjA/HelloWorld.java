74783c670b4182d139afdbb8416a61b3454d26ae
JasonReaume <jason.reaume@charter.net>
2024-02-18 20:57:04 -0500

    Added println statement

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Howdy Github Project");
+		System.out.println("Another println statement added to my HeloWorld.main()");
 	}
 
 }
