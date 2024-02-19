62d4ff3ef8448cabdad1e8287bfe48c2aaea91e5
JasonReaume <jason.reaume@charter.net>
2024-02-18 21:44:17 -0500

    UserA makes new change to second println statement

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Howdy Github Project");
-		System.out.println("Alternate println statement added to my HeloWorld.main()");
+		System.out.println("Alternate println statement added to my HeloWorld.main() with a couple extra words");
 	}
 
 }
